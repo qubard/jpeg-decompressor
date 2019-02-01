b803118d68a2a00b04fc1d878edd1a32e7b37f92
qubard <qubard@gmail.com>
2019-02-01 14:19:14 -0500

    maybe

--- src/network/decompressor.py
+++ src/network/decompressor.py
@@ -11,15 +11,21 @@ class DecompressorNetwork():
 
     # Build the network architecture
     def build(self):
-        self.c1 = conv2d(tf.random.normal([5, 32, 32, 3]), 3, 3)
+        x = tf.placeholder(tf.float32, shape=[None, 32, 32, 3], name='x')  # batch_size x 32 x 32 x 3 input (rgb)
+        cl_1 = conv2d(x, 3, 3, 64, name='c1')  # batch size x 30 x 30 x 64 feature maps\
+        bn_1 = tf.nn.elu(tf.layers.batch_normalization(cl_1, trainable=True))
+
+        pool = tf.nn.max_pool(bn_1, [1, 2, 2, 1], [1, 2, 2, 1], padding='VALID')
+        cl_2 = conv2d(pool, 2, 64, 128, name='c2', stride=[1, 2, 2, 1])
+        bn_2 = tf.nn.elu(tf.layers.batch_normalization(cl_2, trainable=True))
+
         # Initialize all variables
         self.sess.run(tf.global_variables_initializer())
 
-    def test(self):
-        actual_out = self.sess.run(self.c1)
-        print(actual_out.shape)
+        actual_out = self.sess.run(bn_2, feed_dict={x: self.sess.run(tf.random.normal([1, 32, 32, 3]))})
+
+        print(actual_out, actual_out.shape)
+
 
 test = DecompressorNetwork()
 test.build()
-
-test.test()
--- src/network/ops.py
+++ src/network/ops.py
@@ -1,8 +1,9 @@
 import tensorflow as tf
 
 
-def conv2d(x, kernel_size, out_channels, stride=[1,1,1,1]):
-    w = tf.get_variable("w", shape=[kernel_size, kernel_size, 3, out_channels],
-                        initializer=tf.contrib.layers.xavier_initializer())
-    b = tf.get_variable("b", [out_channels], initializer=tf.constant_initializer(0.0))
-    return tf.nn.conv2d(x, w, stride, "SAME")
+def conv2d(x, kernel_size, in_channels, out_channels, name, stride=[1,1,1,1]):
+    with tf.variable_scope(name):
+        w = tf.get_variable("w", shape=[kernel_size, kernel_size, in_channels, out_channels],
+                            initializer=tf.contrib.layers.xavier_initializer())
+        b = tf.get_variable("b", [out_channels], initializer=tf.constant_initializer(0.0))
+        return tf.nn.conv2d(x, w, stride, "VALID")
