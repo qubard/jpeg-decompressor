c177c46938d3ab47679dd2d9be06229e633a7bc7
qubard <qubard@gmail.com>
2019-02-02 05:33:02 -0500

    working compression code

--- run.py
+++ run.py
@@ -24,13 +24,35 @@ f, ax = plt.subplots(n_rows, 2)
 
 start = np.random.randint(low=0, high=499)
 
+from PIL import Image
+import io
+
+# Compress the numpy array as an image to a JPEG
+def compress_numpy_image(arr, quality=50):
+    im = Image.fromarray(np.uint8(255 * arr))
+    out = io.BytesIO()
+    im.save(out, format='JPEG', quality=quality)
+    obj = Image.open(out)
+    compressed = np.zeros(arr.shape)
+
+    # Copy each pixel over (no idea if PIL supports np.array copies)
+    for x in range(0, arr.shape[0]):
+        for y in range(0, arr.shape[1]):
+            pixel = obj.getpixel((x, y))
+            for channel in range(0, len(pixel)):
+                compressed[y][x][channel] = pixel[channel] / 255
+
+    return compressed
+
 for index in range(0, n_rows):
     output = np.reshape(recovered_xes[start + index], [32, 32, 3])
 
     ax[index][0].imshow(arr[start + index])
     ax[index][0].set_title('Original')
 
-    ax[index][1].imshow(output)
-    ax[index][1].set_title('Reconstruction')
+    compressed = compress_numpy_image(arr[start + index], quality=30)
+
+    ax[index][1].imshow(compressed)
+    ax[index][1].set_title('Compressed')
 
 plt.show()
