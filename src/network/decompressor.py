01355ee38495c5cd2e36657da4108608dc9bfbb4
qubard <qubard@gmail.com>
2019-02-09 12:04:28 -0500

    better checkpointing

--- src/network/decompressor.py
+++ src/network/decompressor.py
@@ -1,22 +1,35 @@
 import tensorflow as tf
 import numpy as np
+import os
 
 from .ops import conv2d
 
 
 class DecompressorNetwork():
 
-    def __init__(self, batch_size, learning_rate=1e-3):
+    def __init__(self, batch_size, learning_rate=1e-3, checkpoint_dir="checkpoints/"):
         self.batch_size = batch_size
         self.sess = tf.Session()
         self.learning_rate = learning_rate
+        self.checkpoint_dir = checkpoint_dir
 
         # Generate the model
         self.model = self.build()
         self.saver = tf.train.Saver()
 
     def restore_checkpoint(self, checkpoint):
-        self.saver.restore(self.sess, "checkpoints/model%s.ckpt" % checkpoint)
+        self.restore_last_checkpoint(checkpoint)
+
+    def restore_last_checkpoint(self, checkpoint=None):
+        ckpt = tf.train.get_checkpoint_state(self.checkpoint_dir)
+        if ckpt and ckpt.model_checkpoint_path:
+            ckpt_name = os.path.basename(ckpt.model_checkpoint_path)
+            if checkpoint:
+                ckpt_name = "model%s.ckpt" % checkpoint
+            self.saver.restore(self.sess, os.path.join(self.checkpoint_dir, ckpt_name))
+            return True
+        else:
+            return False
 
     # Evaluate the tensor x on the model
     def eval(self, x):
@@ -27,20 +40,20 @@ class DecompressorNetwork():
         self.y = tf.placeholder(tf.float32, shape=[self.batch_size, 32, 32, 3], name='y')
         self.x = tf.placeholder(tf.float32, shape=[self.batch_size, 32, 32, 3], name='x')  # batch_size x 32 x 32 x 3 input (rgb)
 
-        cl_0 = conv2d(self.x, 3, 128, padding="SAME", name='cl0')
+        cl_0 = conv2d(self.x, 3, 32, padding="SAME", name='cl0')
         bn_0 = tf.nn.elu(tf.layers.batch_normalization(cl_0, trainable=True))
-        cl_1 = conv2d(bn_0, 3, 64, padding="SAME", name='cl1')
+        cl_1 = conv2d(bn_0, 3, 16, padding="SAME", name='cl1')
 
         bn_1 = tf.nn.elu(tf.layers.batch_normalization(cl_1, trainable=True))
 
-        cl_2 = conv2d(bn_1, 3, 64, padding="SAME", name='cl2')
+        cl_2 = conv2d(bn_1, 3, 16, padding="SAME", name='cl2')
         bn_2 = tf.nn.elu(tf.layers.batch_normalization(cl_2, trainable=True))
 
-        cl_3 = conv2d(bn_2, 2, 32, padding="SAME", name='cl3')
+        cl_3 = conv2d(bn_2, 2, 8, padding="SAME", name='cl3')
         bn_3 = tf.layers.batch_normalization(cl_3, trainable=True)
 
         cl_4 = conv2d(bn_3, 2, 3, padding="SAME", name='cl4', activation=tf.nn.sigmoid)
-        bn_4 = tf.nn.relu(self.x + tf.layers.batch_normalization(cl_4, trainable=True)) # residual connection
+        bn_4 = tf.nn.relu(tf.layers.batch_normalization(cl_4, trainable=True))
 
         self.loss = tf.losses.mean_squared_error(self.y, bn_4)
         self.train_op = tf.train.AdamOptimizer(learning_rate=self.learning_rate).minimize(self.loss)
